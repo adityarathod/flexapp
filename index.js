7d91310d9f5a5f7c039d0461690f09b78d0f7f43
applecrazy <applecrazy@users.noreply.github.com>
2018-09-22 14:33:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		isLoggedIn: false,
+		isLoading: false,
+		username: '',
+		password: '',
+		appointments: []
+	},
+	computed: {
+		formPayload: () => {
+			return {
+				username: app.username,
+				password: app.password
+			}
+		}
+	},
+	filters: {
+		removeStopWords: (value) => {
+			var result = value
+			var stopWords = [
+				{ from: 'You made an ', to: '' },
+				{ from: 'Flex Time -', to: 'Flex - ' }
+			]
+			for (var i = 0; i < stopWords.length; i++) {
+				result = result.replace(stopWords[i].from, stopWords[i].to)
+			}
+			return result.trim()
+		},
+		capitalize: (value) => {
+			if (!value) return ''
+			value = value.toString()
+			console.log(value.charAt(0))
+			return value.charAt(0).toUpperCase() + value.slice(1)
+		}
+	},
+	methods: {
+		login: () => {
+			app.isLoading = true
+			fetch('https://flextimes.herokuapp.com/irvington/appointments', {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(app.formPayload)
+			})
+				.then(res => res.json())
+				.then(json => {
+					app.isLoggedIn = true
+					app.isLoading = false
+					app.appointments = json
+				})
+		},
+		logout: () => {
+			app.isLoggedIn = false
+			app.appointments = []
+		}
+	}
+})
+
+
+
+// navbar toggle
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+
+		// Add a click event on each of them
+		$navbarBurgers.forEach(el => {
+			el.addEventListener('click', () => {
+
+				// Get the target from the "data-target" attribute
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
+
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+
+			});
+		});
+	}
+
+});
