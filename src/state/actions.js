270c16896c89a0e13090ee7710d5b93976821cac
applecrazy <applecrazy@users.noreply.github.com>
2018-10-06 22:36:55 -0700

    Add basic redux action scaffolding

---
+++ src/state/actions.js
@@ -0,0 +1,22 @@
+
+export const NEW_APPT = 'NEW_APPT'
+export const DELETE_APPT = 'DELETE_APPT'
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
+
+
+export function newAppt() {
+	return
+}
+
+export function deleteAppt() {
+	return
+}
+
+export function login() {
+	return
+}
+
+export function logout() {
+	return
+}
