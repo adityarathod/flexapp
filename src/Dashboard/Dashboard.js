9079e25db3c61c98d8ed0bef45f699817fa86c8d
applecrazy <applecrazy@users.noreply.github.com>
2018-10-07 21:18:41 -0700

    Add dashboard component

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				hi
+			</React.Fragment>
+		)
+	}
+}
+
+export default Dashboard;
