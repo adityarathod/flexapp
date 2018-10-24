e3560593187980293b8bc9a688a6930820bee74d
applecrazy <applecrazy@users.noreply.github.com>
2018-10-23 23:20:32 -0700

    Add basic structure of home screen

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 
 import IntroView from '../IntroView/IntroView';
 import Onboarding from '../Onboarding/Onboarding';
+import Home from '../Home/Home';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={IntroView} />
           <Route path="/onboarding" component={Onboarding} />
+          <Route path="/home" component={Home} />
         </main>
       </Router>
     );
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 // import { MemoryRouter as Router, Route } from 'react-router-dom';
 
-import Onboarding from '../Onboarding/Onboarding';
+import ScreenHeader from './ScreenHeader';
 
 class Home extends Component {
 	render() {
 		return (
-			<Onboarding />
+			<section className="section" style={{ paddingTop: '18px' }}>
+				<div className="container">
+					<ScreenHeader subtitle="Today, July 4" title="Appointments" />
+					<div>
+						<div className="card" style={{ background: "linear-gradient(125.18deg, #00cd86 0.83%, #3ab52a 98.75%)", boxShadow: "0px 16px 16px rgba(0, 0, 0, 0.16)", borderRadius: "14px", width: '100%', height: '194px' }}>
+
+						</div>
+					</div>
+				</div>
+			</section>
 		)
 	}
 }
