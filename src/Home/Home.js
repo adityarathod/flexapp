23bf6a7953d1b66ace7f1c0af63435f1e2d4da74
applecrazy <applecrazy@users.noreply.github.com>
2018-10-26 22:51:39 -0700

    Update Home to use new AppointmentCard component
    Update the layout as in prototype to use AppointmentCard compnent

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { MemoryRouter as Router, Route } from 'react-router-dom';
 
 import ScreenHeader from './ScreenHeader';
+import AppointmentCard from './AppointmentCard';
 
 class Home extends Component {
 	render() {
@@ -10,9 +11,15 @@ class Home extends Component {
 				<div className="container">
 					<ScreenHeader subtitle="Today, July 4" title="Appointments" />
 					<div>
-						<div className="card" style={{ background: "linear-gradient(125.18deg, #00cd86 0.83%, #3ab52a 98.75%)", boxShadow: "0px 16px 16px rgba(0, 0, 0, 0.16)", borderRadius: "14px", width: '100%', height: '194px' }}>
-
-						</div>
+						<AppointmentCard title="Appointment with Berbawy for FLEX" subtitle="Today, October 17" color="linear-gradient(125.18deg, #00cd86 0.83%, #3ab52a 98.75%)" />
+						<AppointmentCard title="Senior Class Dance Practice" subtitle="Tomorrow, October 18" color="linear-gradient(125.18deg, #FC881B 0.83%, #F22553 98.75%)" />
+						<AppointmentCard title="Chemistry and Physics Help" subtitle="Wednesday, December 20" color="linear-gradient(125.18deg, #885AFF 0.83%, #1749E5 98.75%)" />
+						<AppointmentCard title="We The People &#x000D; Practice" subtitle="Thursday, December 21" color="linear-gradient(125.18deg, #00C8C8 0.83%, #0A9EC4 98.75%)" />
+					</div>
+					<div style={{
+						position: 'fixed', background: 'rgba(248, 248, 248, 0.92)', backdropFilter: 'blur(21.87px)', left: '0px', bottom: '0px', width: '100%', height: '50px'
+					}}>
+						hi
 					</div>
 				</div>
 			</section>
