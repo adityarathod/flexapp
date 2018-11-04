b4c3bfbccc62a41673efd984cd2bb2f75347d23a
applecrazy <applecrazy@users.noreply.github.com>
2018-11-04 12:16:49 -0800

    Add redirect on Welcome page

--- src/components/screens/Onboarding/Welcome.js
+++ src/components/screens/Onboarding/Welcome.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+
+
+import { connect } from 'react-redux';
 
 import './Welcome.scss';
 
-const Welcome = () => (
-	<React.Fragment>
-		<div className="has-text-centered Onboarding-Welcome-buttonContainer">
-			<Link to="/onboarding/login">
-				<button className="button is-link Onboarding-Welcome-blueButton">
-					Get Started
+const Welcome = props => {
+	if (props.credentials.username && props.credentials.password) {
+		return (
+			<Redirect to="/onboarding/login" />
+		)
+	}
+	return (
+		<React.Fragment>
+			<div className="has-text-centered Onboarding-Welcome-buttonContainer">
+				<Link to="/onboarding/login">
+					<button className="button is-link Onboarding-Welcome-blueButton">
+						Get Started
 				</button>
-			</Link>
-		</div>
-		<div className="hero is-fullheight">
-			<div className="hero-body Onboarding-Welcome-heroBody">
-				<div className="has-text-centered" style={{ width: '100%' }}>
-					<h1 className="subtitle Onboarding-Welcome-subtitle">welcome to</h1>
-					<img src="logo.svg" width="100" alt="FLEX logo" />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
+				</Link>
+			</div>
+			<div className="hero is-fullheight">
+				<div className="hero-body Onboarding-Welcome-heroBody">
+					<div className="has-text-centered" style={{ width: '100%' }}>
+						<h1 className="subtitle Onboarding-Welcome-subtitle">welcome to</h1>
+						<img src="logo.svg" width="100" alt="FLEX logo" />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+					</div>
 				</div>
 			</div>
-		</div>
-	</React.Fragment>
-)
+		</React.Fragment>
+	)
+}
 
-export default Welcome;
+const mapStateToProps = state => {
+	return {
+		credentials: state.credentials
+	}
+}
+
+export default connect(mapStateToProps)(Welcome);
